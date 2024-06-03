4b415d8fc56f0a391373debd4e881cecc9012140
Minh <nguyenngocminh190901@gmail.com>
2024-06-03 23:57:54 +0700

    update

---
+++ src/main/java/com/haxxu/movies/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.haxxu.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
